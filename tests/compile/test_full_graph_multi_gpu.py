5e40b78ae0fe858e302b13ec53239f647acd6916
kevin314 <kevlin3141@gmail.com>
2024-09-25 05:57:50 +0000

    Add --streamed-fork option manually

--- tests/compile/test_full_graph_multi_gpu.py
+++ tests/compile/test_full_graph_multi_gpu.py
@@ -3,14 +3,12 @@ import pytest
 from vllm.compilation.backends import vllm_backend
 from vllm.utils import cuda_device_count_stateless
 
-from ..utils import fork_new_process_for_each_test
 from .utils import TEST_MODELS_SMOKE, check_full_graph_support
 
 
 @pytest.mark.parametrize("model_info", TEST_MODELS_SMOKE)
 @pytest.mark.parametrize("tp_size", [2])
 @pytest.mark.parametrize("backend", ["eager", vllm_backend])
-@fork_new_process_for_each_test
 def test_full_graph_multi_gpu(model_info, tp_size, backend):
     model = model_info[0]
     model_kwargs = model_info[1]
